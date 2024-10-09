ce15a82afbae67050496bdd9357a937cd0cd7203
polymnie-io <laurent@polymnie.io>
2024-10-09 18:32:42 +0200

    Add database connector

--- infraDon2/src/views/AboutView.vue
+++ infraDon2/src/views/AboutView.vue
@@ -1,7 +1,46 @@
+<script lang="ts">
+import PouchDB from 'pouchdb'
+
+export default {
+  data() {
+    return {
+      total: 0,
+      data: [] as any[],
+      storage: null as PouchDB.Database | null,
+    };
+  },
+
+  mounted() {
+    this.initDatabase();
+    this.fetchData();
+  },
+
+  methods: {
+
+    fetchData() {
+    },
+
+    initDatabase() {
+      const $storage = new PouchDB('http://admin:admin@localhost:5984/post');
+      if ($storage) {
+        console.log("Connected to collection 'post'");
+      } else {
+        console.warn("Something went wrong");
+      }
+      this.storage = $storage;
+    }
+  },
+
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>Nombre de document : {{ data.length }}</h1>
+  <ul>
+    <li v-for="d in data" :key="d.id">
+      {{ d }}
+    </li>
+  </ul>
 </template>
 
 <style>
@@ -11,5 +50,10 @@
     display: flex;
     align-items: center;
   }
+}
+
+.ucfirst:first-letter {
+  text-transform: uppercase;
+
 }
 </style>
